3884ca8929f0f3f6c030c2f92c8d4253520417b8
Justin Guy <me@justinguy.com>
2015-12-03 20:35:13 +0000

    Cleaned up blank lines in code

--- retweet-server.py
+++ retweet-server.py
@@ -15,37 +15,31 @@ api = tweepy.API(auth)
 me = api.me()
 
 
-
 @app.route('/info')
 @payment.required(INFO_PRICE)
 def info():
-
     return "\n______\n\nThis endpoint is for Twitter account @%s which has %d followers.\nYou can purchase a follow from @%s for %d satoshi by calling \n/follow/YOUR_TWITTER_HANDLE or a retweet for %d satoshi at /retweet/TWEET_ID or a favorite for %d satoshi at /favorite/TWEET_ID.\n______\n" % (me.screen_name, me.followers_count, me.screen_name, FOLLOW_PRICE, RETWEET_PRICE, FAVORITE_PRICE)
-
 
 
 @app.route('/follow/<acct_name>')
 @payment.required(FOLLOW_PRICE)
-def follow(acct_name):
-    
+def follow(acct_name):    
     res = api.create_friendship(acct_name)
     return "@%s was successfully followed by @%s" % (acct_name, me.screen_name)
+
 
 @app.route('/favorite/<int:tweet_id>')
 @payment.required(FAVORITE_PRICE)
 def favorite(tweet_id):
-    
     res = api.create_favorite(tweet_id)
     return "Tweet successfully favorited by @%s" % (me.screen_name)
 
 
 @app.route('/retweet/<int:tweet_id>')
 @payment.required(RETWEET_PRICE)
-def retweet(tweet_id):
-    
+def retweet(tweet_id):    
     res = api.retweet(tweet_id)
     return "Successfully retweeted by @%s (https://twitter.com/%s)" % (me.screen_name, me.screen_name)
-
 
 
 if __name__ == '__main__':
